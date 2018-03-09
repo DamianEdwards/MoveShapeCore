3e4df2142c5ac054fd5a0caaabac26e3e1878874
damianedwards <damian@damianedwards.com>
2018-03-08 16:20:54 -0800

    Initial commit

---
+++ MoveShape21/MouseMoveHub.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace MoveShape21
+{
+    public class MouseMoveHub : Hub<IMoveShapeClient>
+    {
+        public Task UpdatePosition(Point position)
+        {
+            return Clients.Others.PositionChanged(position);
+        }
+    }
+
+    public interface IMoveShapeClient
+    {
+        Task PositionChanged(Point position);
+    }
+
+    public class Point
+    {
+        public Point() { }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+    }
+}
---
+++ MoveShape21/wwwroot/js/MoveShape.js
@@ -0,0 +1,56 @@
+﻿/// <reference path="../../node_modules/@aspnet/signalr/dist/esm/index.d.ts" />
+/// <reference path="../../node_modules/@aspnet/signalr-protocol-msgpack/dist/esm/index.d.ts" />
+(async function () {
+    'use strict';
+    const protocol = new signalR.protocols.msgpack.MessagePackHubProtocol(),
+        hub = new signalR.HubConnection('/moveshape', { protocol: protocol }),
+        shape = document.getElementById('shape');
+
+    function updatePos(x, y) {
+        shape.style.left = x.toString() + 'px';
+        shape.style.top = y.toString() + 'px';
+    }
+
+    hub.on('PositionChanged', position => {
+        console.log('Incoming position update: ' + position.x + ', ' + position.y);
+        updatePos(position.x, position.y);
+    });
+
+    await hub.start();
+
+    let mouseDown = false,
+        offset = { x: 0, y: 0 };
+
+    shape.addEventListener('touchstart', start);
+    shape.addEventListener('mousedown', start);
+
+    function start(e) {
+        let data = e.touches ? e.touches.item(0) : e;
+        offset.x = data.clientX - shape.offsetLeft;
+        offset.y = data.clientY - shape.offsetTop;
+        console.log('start: x=' + data.clientX + ', y=' + data.clientY + ', shapeLeft=' + shape.offsetLeft + ', shapeTop=' + shape.offsetTop);
+        mouseDown = true;
+    }
+
+    document.addEventListener('touchend', end);
+    document.addEventListener('mouseup', end);
+
+    function end(e) {
+        console.log('end');
+        mouseDown = false;
+    }
+
+    document.addEventListener('touchmove', move);
+    document.addEventListener('mousemove', move);
+
+    function move(e) {
+        if (mouseDown) {
+            let data = e.touches ? e.touches.item(0) : e,
+                newPos = { X: data.clientX - offset.x, Y: data.clientY - offset.y };
+            // BUG: Have to send to server w/ PascalCase props when using MsgPack
+            updatePos(newPos.X, newPos.Y);
+            hub.invoke('UpdatePosition', newPos);
+            console.log('move');
+        }
+    }
+}());
